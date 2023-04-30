85bec1b6d4ab623dd6801e5e84cd3ccf32a4adc6
Ellen Helena <ellen.kerf@appliedtechnology.se>
2023-04-30 14:13:53 +0200

    fixed signIn signOut navigation

--- client/src/components/auth/authDetails.tsx
+++ client/src/components/auth/authDetails.tsx
@@ -2,8 +2,10 @@ import type { User } from "firebase/auth";
 import { onAuthStateChanged, signOut } from "firebase/auth";
 import { auth } from "../../services/firebase";
 import { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
 
 const AuthDetails = () => {
+  const navigate = useNavigate();
   const [authUser, setAuthUser] = useState<User | null>(null);
 
   useEffect(() => {
@@ -24,6 +26,7 @@ const AuthDetails = () => {
     signOut(auth)
       .then(() => {
         console.log("sign out successful");
+        navigate('/');
       })
       .catch((error) => console.log(error));
   };
@@ -42,3 +45,49 @@ const AuthDetails = () => {
 };
 
 export default AuthDetails;
+
+
+// import type { User } from "firebase/auth";
+// import { onAuthStateChanged, signOut } from "firebase/auth";
+// import { auth } from "../../services/firebase";
+// import { useEffect, useState } from "react";
+
+// const AuthDetails = () => {
+//   const [authUser, setAuthUser] = useState<User | null>(null);
+
+//   useEffect(() => {
+//     const listen = onAuthStateChanged(auth, (user) => {
+//       if (user) {
+//         setAuthUser(user);
+//       } else {
+//         setAuthUser(null);
+//       }
+//     });
+
+//     return () => {
+//       listen();
+//     };
+//   }, []);
+
+//   const userSignOut = () => {
+//     signOut(auth)
+//       .then(() => {
+//         console.log("sign out successful");
+//       })
+//       .catch((error) => console.log(error));
+//   };
+
+//   return (
+//     <div>
+//       {authUser ? (
+//         <>
+//           <p>{`Signed In`}</p><button onClick={userSignOut}>Sign Out</button>
+//         </>
+//       ) : (
+//         <p>Signed Out</p>
+//       )}
+//     </div>
+//   );
+// };
+
+// export default AuthDetails;
